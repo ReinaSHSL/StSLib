2428e021e66de32746858081cecb4c07959eb0a0
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-17 22:58:26 -0800

    Create preview card

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
@@ -1,7 +1,11 @@
 package com.evacipated.cardcrawl.mod.stslib.patches;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 
@@ -22,5 +26,19 @@ public class BranchingUpgradesPatch {
     public static class BranchingUpgradePreviewCardField {
         public static SpireField<AbstractCard> branchUpgradePreviewCard = new SpireField<>(() -> null);
     }
-    
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = "render"
+    )
+    public static class RenderBranchingUpgrade {
+        public static SpireReturn Prefix(GridCardSelectScreen __instance, SpriteBatch sb) {
+            AbstractCard c = (AbstractCard) ReflectionHacks.getPrivate(__instance, GridCardSelectScreen.class, "hoveredCard");
+            if (c instanceof BranchingUpgradesCard) {
+                BranchingUpgradesCard previewCard = (BranchingUpgradesCard) BranchingUpgradePreviewCardField.branchUpgradePreviewCard.get(__instance);
+                previewCard.branchUpgrade();
+            }
+            return SpireReturn.Continue();
+        }
+    }
 }
