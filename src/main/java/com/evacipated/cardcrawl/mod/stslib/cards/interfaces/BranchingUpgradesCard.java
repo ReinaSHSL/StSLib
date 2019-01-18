ca6086d02a008c5445f451cae7a271c7992d1378
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-17 23:00:14 -0800

    display upgrades

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/cards/interfaces/BranchingUpgradesCard.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/cards/interfaces/BranchingUpgradesCard.java
@@ -15,4 +15,22 @@ public interface BranchingUpgradesCard {
             branchUpgrade();
         }
     }
+
+    default void displayBranchUpgrades() {
+        if (this instanceof AbstractCard) {
+            AbstractCard c = (AbstractCard) this;
+            if (c.upgradedCost) {
+                c.isCostModified = true;
+            }
+            if (c.upgradedBlock) {
+                c.isBlockModified = true;
+            }
+            if (c.upgradedDamage) {
+                c.isDamageModified = true;
+            }
+            if (c.upgradedMagicNumber) {
+                c.isMagicNumberModified = true;
+            }
+        }
+    }
 }
