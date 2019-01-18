e9cbdce24ef7d8348bcbb64ff663b8cfb8d0877a
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-17 23:41:39 -0800

    Render logic

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 import javassist.CtBehavior;
 
@@ -48,15 +50,67 @@ public class BranchingUpgradesPatch {
             }
         }
 
-        private static class Locator extends SpireInsertLocator
-        {
+        private static class Locator extends SpireInsertLocator {
             @Override
-            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
-            {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
                 Matcher finalMatcher = new Matcher.MethodCallMatcher("com.megacrit.cardcrawl.cards.AbstractCard", "makeStatEquivalentCopy");
                 return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
             }
         }
     }
 
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = "render"
+    )
+    public static class RenderBranchingUpgrade {
+        @SpireInsertPatch(
+                locator = Locator.class
+        )
+        public static SpireReturn Insert(GridCardSelectScreen __instance, SpriteBatch sb) {
+            AbstractCard c = (AbstractCard) ReflectionHacks.getPrivate(__instance, GridCardSelectScreen.class, "hoveredCard");
+            if (c instanceof BranchingUpgradesCard) {
+                AbstractCard branchUpgradedCard = BranchingUpgradePreviewCardField.branchUpgradePreviewCard.get(__instance);
+                c.current_x = (Settings.WIDTH * 0.36F);
+                c.current_y = (Settings.HEIGHT / 2.0F);
+                c.target_x = (Settings.WIDTH * 0.36F);
+                c.target_y = (Settings.HEIGHT / 2.0F);
+                c.render(sb);
+                c.updateHoverLogic();
+                __instance.upgradePreviewCard.current_x = (Settings.WIDTH * 0.63F);
+                __instance.upgradePreviewCard.current_y = (Settings.HEIGHT / 2.0F);
+                __instance.upgradePreviewCard.target_x = (Settings.WIDTH * 0.63F);
+                __instance.upgradePreviewCard.target_y = (Settings.HEIGHT / 2.0F);
+                __instance.upgradePreviewCard.render(sb);
+                __instance.upgradePreviewCard.updateHoverLogic();
+                __instance.upgradePreviewCard.renderCardTip(sb);
+                branchUpgradedCard.current_x = (Settings.WIDTH * 0.8F);
+                branchUpgradedCard.current_y = (Settings.HEIGHT / 2.0F);
+                branchUpgradedCard.target_x = (Settings.WIDTH * 0.63F);
+                branchUpgradedCard.target_y = (Settings.HEIGHT / 2.0F);
+                branchUpgradedCard.render(sb);
+                branchUpgradedCard.updateHoverLogic();
+                branchUpgradedCard.renderCardTip(sb);
+                if ((__instance.forUpgrade) || (__instance.forTransform) || (__instance.forPurge) || (__instance.isJustForConfirming) || (__instance.anyNumber)) {
+                    __instance.confirmButton.render(sb);
+                }
+                CardGroup targetGroup = (CardGroup) ReflectionHacks.getPrivate(__instance, GridCardSelectScreen.class, "targetGroup");
+                String tipMsg = (String) ReflectionHacks.getPrivate(__instance, GridCardSelectScreen.class, "tipMsg");
+                if ((!__instance.isJustForConfirming) || (targetGroup.size() > 5)) {
+                    com.megacrit.cardcrawl.helpers.FontHelper.renderDeckViewTip(sb, tipMsg, 96.0F * Settings.scale, Settings.CREAM_COLOR);
+                }
+                return SpireReturn.Return(null);
+            }
+            return SpireReturn.Continue();
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher("com.megacrit.cardcrawl.screens.select.GridCardSelectScreen", "renderArrows");
+                return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
+            }
+        }
+    }
 }
