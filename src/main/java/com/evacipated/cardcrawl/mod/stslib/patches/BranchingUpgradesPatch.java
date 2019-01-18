ffd52d86ba569abdd21020cd469662f989d86b30
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-17 22:56:40 -0800

    branch upgrade preview card field

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
@@ -1,8 +1,13 @@
 package com.evacipated.cardcrawl.mod.stslib.patches;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesInterface;
 import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 
 public class BranchingUpgradesPatch {
 
@@ -13,4 +18,13 @@ public class BranchingUpgradesPatch {
     public static class BranchingUpgradeField {
         public static SpireField<Boolean> isBranchUpgraded = new SpireField<>(() -> false);
     }
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = SpirePatch.CLASS
+    )
+    public static class BranchingUpgradePreviewCardField {
+        public static SpireField<AbstractCard> branchUpgradePreviewCard = new SpireField<>(() -> null);
+    }
+
 }
