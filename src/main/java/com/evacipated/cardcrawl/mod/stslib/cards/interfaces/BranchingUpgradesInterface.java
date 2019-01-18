eeb244e9bf1660d4fa65b9d91787863ff7928ecb
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-17 22:46:14 -0800

    Branching Upgrades interface

---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/cards/interfaces/BranchingUpgradesInterface.java
@@ -0,0 +1,18 @@
+package com.evacipated.cardcrawl.mod.stslib.cards.interfaces;
+
+import com.evacipated.cardcrawl.mod.stslib.patches.BranchingUpgradesPatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public interface BranchingUpgradesInterface {
+
+    void branchUpgrade();
+
+    default void setIsBranchUpgrade() {
+        if (this instanceof AbstractCard) {
+            AbstractCard c = (AbstractCard) this;
+            BranchingUpgradesPatch.BranchingUpgradeField.isBranchUpgraded.set(c, true);
+            c.upgraded = true;
+            branchUpgrade();
+        }
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/BranchingUpgradesPatch.java
@@ -0,0 +1,16 @@
+package com.evacipated.cardcrawl.mod.stslib.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class BranchingUpgradesPatch {
+
+    @SpirePatch(
+            clz = AbstractCard.class,
+            method = SpirePatch.CLASS
+    )
+    public static class BranchingUpgradeField {
+        public static SpireField<Boolean> isBranchUpgraded = new SpireField<>(() -> false);
+    }
+}
